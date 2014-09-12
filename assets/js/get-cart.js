226d452ba7511c9f1b361c356a32f7718ef448b3
Michał Kurzeja <mkurzeja@accesto.pl>
2014-09-12 09:36:11 +0200

    Added banners, reformated code

--- assets/js/get-cart.js
+++ assets/js/get-cart.js
@@ -1,10 +1,10 @@
-jQuery( function( $ ) {
-    $('body').on('added_to_cart', function() {
+jQuery(function ($) {
+    $('body').on('added_to_cart', function () {
         var data = {
             action: 'woocommerce_piwik_get_cart'
         };
 
-        $.post(wc_add_to_cart_params.ajax_url, data, function(cartItems) {
+        $.post(wc_add_to_cart_params.ajax_url, data, function (cartItems) {
             var arrayLength = cartItems.length, revenue = 0;
             for (var i = 0; i < arrayLength; i++) {
                 _paq.push(['addEcommerceItem',
--- woocommerce-piwik.php
+++ woocommerce-piwik.php
@@ -1,24 +1,22 @@
 <?php
-
 /**
-Plugin Name: WooCommerce Piwik
-Plugin URI: http://wordpress.org/plugins/woocommerce-piwik/
-Description: Allows Piwik and Piwik PRO tracking code to be inserted into WooCommerce store pages.
-Author: Piwik PRO
-Author URI: http://www.piwik.pro
-Version: 1.0.0
-*/
+ * Plugin Name: WooCommerce Piwik integration
+ * Plugin URI: http://wordpress.org/plugins/woocommerce-piwik-integration/
+ * Description: Allows Piwik and Piwik PRO tracking code to be inserted into WooCommerce store pages.
+ * Author: Piwik PRO
+ * Author URI: http://www.piwik.pro
+ * Version: 2.0.0
+ */
 
-function wc_piwik_add_integration($integrations)
-{
+function wc_piwik_add_integration( $integrations ) {
 	global $woocommerce;
 
-	if (is_object($woocommerce) && version_compare($woocommerce->version, '2.1-beta-1', '>=')) {
-		include_once('includes/class-wc-piwik.php');
+	if ( is_object( $woocommerce ) && version_compare( $woocommerce->version, '2.1-beta-1', '>=' ) ) {
+		include_once( 'includes/class-wc-piwik.php' );
 		$integrations[] = 'WC_Piwik';
 	}
 
 	return $integrations;
 }
 
-add_filter('woocommerce_integrations', 'wc_piwik_add_integration');
+add_filter( 'woocommerce_integrations', 'wc_piwik_add_integration' );
